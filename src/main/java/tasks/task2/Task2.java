15cdcd06b8771b917bb77597a2cc5936a2c67804
Angelina <angelina-262695@mail.ru>
2023-03-01 20:20:35 +0300

    task 2

--- src/main/java/tasks/task2/Task2.java
+++ src/main/java/tasks/task2/Task2.java
@@ -5,23 +5,29 @@ import java.util.Scanner;
 //разделить рулет на n число человек
 public class Task2 {
     public static void getResult() {
-        try (Scanner scanner = new Scanner(System.in)){
-            int n = scanner.nextInt();
-            int startRange = 1;
-            int endRange = 2 * (int)Math.pow(10, 9);
-            int k = 0;
-            int countParts = 1;
-            if (n >= startRange && n <= endRange) {
-                if (n == 1) {
-                    k = 0;
-                } else {
-                    while ( countParts < n) {
-                        k++;
-                        countParts *= 2;
+        int k = 0;
+        try (Scanner scanner = new Scanner(System.in)) {
+            if (scanner.hasNextInt()) {
+                int n = scanner.nextInt();
+                int startRange = 1;
+                int endRange = 2 * (int)Math.pow(10, 9);
+                int countParts = 1;
+                if (n >= startRange && n <= endRange) {
+                    if (n == 1) {
+                        k = 0;
+                    } else {
+                        while ( countParts < n) {
+                            k++;
+                            countParts *= 2;
+                        }
                     }
+                } else {
+                    throw new RuntimeException("Incorrect data");
                 }
-                System.out.println(k);
+            } else {
+                throw new RuntimeException("Incorrect data");
             }
         }
+        System.out.println(k);
     }
 }
